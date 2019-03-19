5a327e1cdc50a3fec06b8be4ad3a042e1faaf9c8
wangyafeng <wangyafeng.159@163.com>
2019-03-19 10:40:41 +0800

    add config file

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    // 如果您不需要生产时的源映射，那么将此设置为false可以加速生产构建
+    productionSourceMap: false,
+    devServer: {
+        port: 8085, // 端口号
+        host: 'localhost',
+        https: false, // https:{type:Boolean}
+        open: true, //配置自动启动浏览器
+        // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
+        proxy: {
+            '/api': {
+                target: '<url>',
+                ws: true,
+                changeOrigin: true
+            },
+            '/foo': {
+                target: '<other_url>'
+            }
+        }, // 配置多个代理
+    }
+}
