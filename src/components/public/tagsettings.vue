b5dca927917b9b2abfe06fd968eafb3cfa62d02d
wangyafeng <wangyafeng.159@163.com>
2019-04-18 11:16:43 +0800

    tagsettings 修改

--- src/components/public/tagsettings.vue
+++ src/components/public/tagsettings.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <ul v-if="tags!=null">
-      <li v-for="(tag,index) in tags" :key="index" class="tag-li">
-        <div>
-          <i :style="{height:'10px',width:'10px',background:tag.bgColor}"></i>
-          <p>{{tag.tagName}}</p>
-        </div>
-        <Icon type="ios-arrow-forward"/>
-      </li>
-    </ul>
+    <div v-if="tags!=null" class="tag-left">
+      <ul>
+        <li v-for="(tag,index) in tags" :key="index" class="tag-li">
+          <div>
+            <i :style="{height:'10px',width:'10px',background:tag.bgColor}"></i>
+            <p>{{tag.tagName}}</p>
+          </div>
+          <Icon type="ios-arrow-forward"/>
+        </li>
+      </ul>
+      <div class="tag-right"></div>
+    </div>
     <div v-else>暂无标签</div>
   </div>
 </template>
@@ -36,6 +39,13 @@ export default {
   &:hover {
     background-color: #e5e5e5;
   }
+}
+.tag-left {
+  width: 200px;
+  float: left;
+}
+.tag-right {
+  float: right;
 }
 </style>
 
