3ff2b5564e113549ec7a0146a5b2eb520ef52407
汪亚锋 <wangyafeng.159@163.com>
2019-04-22 19:33:34 +0800

    修改文件api

--- src/axios/fileApi.js
+++ src/axios/fileApi.js
@@ -12,7 +12,9 @@ export function changeName(fileId, fileName) {
     return fetch({
         url: `/files/${fileId}/name`,
         method: "put",
-        params: {fileName:fileName}
+        params: {
+            fileName: fileName
+        }
     });
 }
 // 下载文件
@@ -27,7 +29,9 @@ export function jionPeople(fileId, newJoin) {
     return fetch({
         url: `/files/${fileId}/add_remove_join`,
         method: "put",
-        params: {newJoin: newJoin}
+        params: {
+            newJoin: newJoin
+        }
     });
 }
 
@@ -36,32 +40,58 @@ export function removeFile(folderId, fileIds) {
     return fetch({
         url: `/files/${folderId}/m_move`,
         method: "put",
-        params: {fileIds: fileIds}
+        params: {
+            fileIds: fileIds
+        }
     });
 }
 // 复制文件
-export function cloneFile(folderId,fileIds) {
+export function cloneFile(folderId, fileIds) {
     return fetch({
         url: '/files/copy',
         method: "post",
         data: {
-            folderId:folderId,
-            fileIds:fileIds
+            folderId: folderId,
+            fileIds: fileIds
         }
     });
 }
 // 文件移到回收站
-export function recycleBin(fileIds,projectId) {
+export function recycleBin(fileIds, projectId) {
     return fetch({
         url: `/files/${fileIds}/m_recycle`,
         method: "put",
-        params: {projectId: projectId}
+        params: {
+            projectId: projectId
+        }
     });
 }
 // 添加关联
 export function addRelation(data) {
     return fetch({
         url: '/bindings',
+        method: "post",
+        data: data
+    });
+}
+
+//更新文件版本
+export function updateFileVersion() {
+
+}
+
+//收藏文件
+export function collectFile(fileId) {
+    return fetch({
+        url: `/files/${fileId}/collect`,
+        method: "post",
+        data: ''
+    });
+}
+//文件隐私模式
+export function filePrivacy(data) {
+    return fetch({
+        url: `/files/${fileId}/privacy`,
         method: "post",
         data: data
     });
