bde863b2ad032dfd9950d10055bbfb4a2656a2d4
DESKTOP-V5P107C\PC <382267044@qq.com>
2019-05-10 12:01:41 +0800

    解决样式问题（导航下拉 层级 新建等 ）

--- src/views/CreateProject.vue
+++ src/views/CreateProject.vue
@@ -112,6 +112,7 @@ export default {
   }
   /deep/.ivu-input {
     height: 40px !important;
+    line-height: 30px;
     font-size: 14px;
   }
   .inputbox {
@@ -130,6 +131,14 @@ export default {
   justify-content: space-between;
   width: 320px;
   margin: 15px auto;
+}
+.create-project-time  .ivu-date-picker{
+  width:150px !important;
+}
+.create-project-time  .ivu-input-icon{
+    
+    height: 40px !important;
+    line-height: 40px !important;
 }
 </style>
 
