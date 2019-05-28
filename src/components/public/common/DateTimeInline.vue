10e5e9f471f86d83c8d89c3d2395fddd7b2dbc84
汪亚锋 <wangyafeng.159@163.com>
2019-05-28 09:28:18 +0800

    Auto stash before merge of "master" and "汪亚锋/master"

--- src/components/public/common/DateTimeInline.vue
+++ src/components/public/common/DateTimeInline.vue
@@ -1,38 +1,22 @@
 <template>
   <div>
-    <DatePicker
-            :open="visible"
-            :value="date"
-            confirm
-            class="datatimeinline"
-            @on-change="dateChange"
-            style="width:216px"
-    >
+    <DatePicker :open="visible" :value="date" confirm class="datatimeinline" @on-change="dateChange" style="width:216px">
       <Row class="picker-input">
         <iCol span="14">
           <input v-model="date" />
         </iCol>
         <iCol span="10">
-          <TimePicker confirm format="HH:mm" :value="time" @on-change="timeChange"
-                      ref='timePicker'>
+          <TimePicker format="HH:mm" :value="time" @on-change="timeChange" ref='timePicker'>
           </TimePicker>
         </iCol>
       </Row>
       <Row class="footer">
         <iCol span="11">
-          <Button
-                  long
-                  @click="clear"
-          >清除</Button>
+          <Button long @click="clear">清除</Button>
         </iCol>
         <iCol span="2"></iCol>
         <iCol span="11">
-          <Button
-                  type="primary"
-                  long
-                  @click="confirm"
-                  :disabled="disabled"
-          >确定</Button>
+          <Button type="primary" long @click="confirm" :disabled="disabled">确定</Button>
         </iCol>
       </Row>
     </DatePicker>
@@ -81,17 +65,17 @@ export default {
     }
   },
   methods: {
-    // selectTimeOk(data){
-    //   console.log(data)
-    // },
-    timeChange (time) {
-      this.time = time
-      this.realDateTime = this.date + ' ' + this.time
-      // this.$refs.timePicker.visible = false
+    selectTimeOk(data) {
+      console.log(data);
+    },
+    timeChange(time) {
+      this.time = time;
+      this.realDateTime = this.date + " " + this.time;
+      this.$refs.timePicker.visible = false;
     },
     dateChange(date) {
       this.date = date;
-      this.realDateTime = new Date(date).getTime()
+      this.realDateTime = new Date(date).getTime();
     },
     init() {
       if (this.datetime) {
@@ -106,7 +90,7 @@ export default {
     },
     clear() {
       this.date = this.$moment(new Date()).format("YYYY-MM-DD");
-      this.realDateTime = new Date(this.date).getTime()
+      this.realDateTime = new Date(this.date).getTime();
       this.$emit("clear");
     },
     confirm() {
@@ -120,42 +104,42 @@ export default {
 </script>
 
 <style lang="less">
-  .picker-input {
-    border: 1px solid #e5e5e5;
-    border-radius: 4px;
-    height: 30px;
-    padding: 5px 0;
-    margin-top: 6px;
-    > div.ivu-col {
-      height: 18px;
-      &:first-child {
-        border-right: 1px solid #e5e5e5;
-      }
-    }
-    input {
-      padding: 0 12px;
-      border: none;
-      outline: none !important;
-      width: 100%;
-      height: 100%;
-      display: block;
+.picker-input {
+  border: 1px solid #e5e5e5;
+  border-radius: 4px;
+  height: 30px;
+  padding: 5px 0;
+  margin-top: 6px;
+  > div.ivu-col {
+    height: 18px;
+    &:first-child {
+      border-right: 1px solid #e5e5e5;
     }
   }
+  input {
+    padding: 0 12px;
+    border: none;
+    outline: none !important;
+    width: 100%;
+    height: 100%;
+    display: block;
+  }
+}
 .datatimeinline {
   display: block;
   height: 324px;
   font-size: 14px;
-  .picker-input{
+  .picker-input {
     position: relative;
-    /deep/ .ivu-select-dropdown{
+    /deep/ .ivu-select-dropdown {
       position: absolute !important;
-      top: 10px!important;
+      top: 10px !important;
     }
-    /deep/ .ivu-picker-confirm{
+    /deep/ .ivu-picker-confirm {
       display: flex !important;
     }
   }
-  /deep/ .ivu-select-dropdown{
+  /deep/ .ivu-select-dropdown {
     position: relative !important;
     top: 0 !important;
     left: 0 !important;
