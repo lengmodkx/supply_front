5227ac5105b0dcb3e804e9566b8df9c57e19586d
苗云宇 <miaoyunyu@preamsolutions.com>
2020-03-05 14:36:07 +0800

    修改BUG

---
+++ src/views/delOrg.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="wrap-div">
+       
+             <img src="../assets/images/delete.png"> 
+    </div>
+</template>
+
+<script>
+import { orgInfo , updateOrg, delOrg,getAllTransfer,postTransfer} from '../axios/api'
+export default {
+    name: "enterpriseInformation",
+    data () {
+        return {
+           
+        }
+    },
+    mounted(){
+        
+    },
+    methods:{
+       
+    },
+    created() {
+        
+    },
+}
+</script>
+
+<style scoped lang="less">
+       
+        .wrap-div{
+           
+            width: 260px;
+            height: 300px;
+            position: absolute;
+            left:50%;
+            top:50%;
+            margin-left: -130px;
+
+            
+
+           img{
+               display: block;
+               width: 100%;
+              
+             
+              
+                
+           }
+        }
+
+</style>
