0558c083c591e4b852eddbc75ecb55cfa11c3c32
苗云宇 <miaoyunyu@preamsolutions.com>
2020-03-05 14:37:06 +0800

    解决冲突

--- vue.config.js
+++ vue.config.js
@@ -41,10 +41,12 @@ module.exports = {
         // host: "localhost",
         https: false, // https:{type:Boolean}
         open: true, //配置自动启动浏览器
-        // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
+        // proxy: 'http://localhost:4000'// 配置跨域处理,只有一个代理
         proxy: {
             "/api": {
-                target: "http://192.168.1.10:8080",
+                target: "http://2v1621n353.51mypc.cn",
+                //target: "http://test.art1001.com/api/",
+                
                 ws: true,
                 changeOrigin: true,
                 pathRewrite: {
