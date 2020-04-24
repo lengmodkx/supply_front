0f4c62aca7f8723eabceac5f1f3140721050e379
杨鲁靖 <yanglujing@art1001.com>
2020-04-24 20:15:37 +0800

    企业头像修改

---
+++ src/utils/ossKey.js
@@ -0,0 +1,9 @@
+function ossksy() {
+
+};
+ossksy.prototype = {
+    region: "oss-cn-beijing",
+    accessKeyId: "LTAIP4MyTAbONGJx",
+    accessKeySecret: "coCyCStZwTPbfu93a3Ax0WiVg3D4EW",
+    bucket: "art1001-bim-5d"
+}
