a83e5d2f560d10affeef9f8566db1af7cf3867a5
wangji <763515562@qq.com>
2020-08-25 11:49:19 +0800

    弹窗

--- src/components/public/instantChat.vue
+++ src/components/public/instantChat.vue
@@ -176,13 +176,13 @@ export default {
   },
   mounted() {
     if (this.changeName == "group") {
-      if( this.userList[this.changeName][0]== undefined ||this.userList[this.changeName][0]== 'undefined'){
-          this.$Message.error('系统异常，请刷新！');
-      }else{
+      // if( this.userList[this.changeName][0]== undefined ||this.userList[this.changeName][0]== 'undefined'){
+      //     this.$Message.error('系统异常，请刷新！');
+      // }else{
           this.getGroupUserList().then((res) => {
             this.select2(this.userList[this.changeName][0], this.userList[this.changeName][0].groupid, 0);
           });
-      }
+      // }
       
     } else {
       this.projectName = this.userInfoList.userEntity.userName;
