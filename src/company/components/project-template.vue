9e82dff9f5a7bc3f4fca1e1c559666ebe43e62a7
wangji <763515562@qq.com>
2021-03-05 10:08:46 +0800

    项目模板封面设置oss

--- src/company/components/project-template.vue
+++ src/company/components/project-template.vue
@@ -32,22 +32,16 @@
           <img :src="defaultImg" />
         </div>
         <div class="add-template-upload" style="margin-top:15px">
-          <Upload
-            action="/"
-            ref="upload"
-            :show-upload-list="false"
-            :format="['jpg', 'jpeg', 'png']"
-            :max-size="1024"
-            :on-format-error="handleFormatError"
-            :on-exceeded-size="handleMaxSize"
-            :before-upload="handleBeforeUpload"
-          >
+          <Upload ref="upload" :show-upload-list="true" :format="['jpg', 'jpeg', 'png']" :max-size="1024"
+            :on-format-error="handleFormatError" :on-exceeded-size="handleMaxSize" :before-upload="handleBeforeUpload"
+            :action="host" :data="uploadData" :headers="headers" :on-success="handleSuccess">
             <Button icon="ios-cloud-upload-outline">上传封面</Button>
           </Upload>
           <span style="margin-left:10px">最佳图片比例为300*150</span>
         </div>
         <div style="margin-top:15px"><Input v-model="templateName" placeholder="请输入模板名称（必填）" /></div>
-        <div style="margin-top:15px"><Input v-model="templateDes" maxlength="100" show-word-limit type="textarea" placeholder="请输入模板描述（必填）" /></div>
+        <div style="margin-top:15px"><Input v-model="templateDes" maxlength="100" show-word-limit type="textarea"
+            placeholder="请输入模板描述（必填）" /></div>
         <div style="margin-top:15px"><Button type="primary" long @click="saveTemplate">保存模板</Button></div>
       </div>
     </Modal>
@@ -87,67 +81,109 @@
   </div>
 </template>
 <script>
-import projectTemplate from "./projectTemplate.vue";
-import { createTemplate, updateTemplate, deleteTemplate, initTemplate } from "../axios/api.js";
-import { initTemplateRelation, createTemplateRelation, updateTemplateRelation, deleteTemplateRelation } from "../axios/api.js";
-import OSS from "ali-oss";
-let client = new OSS({
-  region: "oss-cn-beijing",
-  accessKeyId: "LTAIP4MyTAbONGJx",
-  accessKeySecret: "coCyCStZwTPbfu93a3Ax0WiVg3D4EW",
-  bucket: "art1001-bim-5d",
-});
-export default {
-  components: { projectTemplate },
-  data() {
-    return {
-      defaultImg: "https://art1001-bim-5d.oss-cn-beijing.aliyuncs.com/upload/project/bj.png",
-      loading: false,
-      templates: [],
-      showTemplate: false,
-      templateName: "",
-      templateDes: "",
-      fileName: "",
-      templateId: "",
-      showTaskList: false,
-      relations: [],
-      addRelation: false,
-      relationName: "",
-      relationOrder: 0,
-      relationId: "",
-    };
-  },
-  mounted() {
-    this.initTemplate();
-  },
-  methods: {
-    initTemplate() {
-      initTemplate({ orgId: localStorage.companyId }).then((res) => {
-        if (res.result == 1) {
-          this.templates = res.data;
-        }
-      });
+  import projectTemplate from "./projectTemplate.vue";
+  import {
+    createTemplate,
+    updateTemplate,
+    deleteTemplate,
+    initTemplate
+  } from "../axios/api.js";
+  import {
+    initTemplateRelation,
+    createTemplateRelation,
+    updateTemplateRelation,
+    deleteTemplateRelation
+  } from "../axios/api.js";
+  import {
+    oss
+  } from "@/axios/ossweb";
+  export default {
+    components: {
+      projectTemplate
     },
+    data() {
+      return {
+        defaultImg: "https://art1001-bim-5d.oss-cn-beijing.aliyuncs.com/upload/project/bj.png",
+        loading: false,
+        templates: [],
+        showTemplate: false,
+        templateName: "",
+        templateDes: "",
+        fileName: "",
+        templateId: "",
+        showTaskList: false,
+        relations: [],
+        addRelation: false,
+        relationName: "",
+        relationOrder: 0,
+        relationId: "",
+        uploadData: {},
+        host: "",
+        headers: {
+          "x-auth-token": localStorage.token
+        },
+        fileObj:{}
+      };
+    },
+    mounted() {
+      this.initTemplate();
+    },
+    methods: {
+      initTemplate() {
+        initTemplate({
+          orgId: localStorage.companyId
+        }).then((res) => {
+          if (res.result == 1) {
+            this.templates = res.data;
+          }
+        });
+      },
 
-    addTemplate(item) {
-      this.showTemplate = true;
-      if (item) {
-        this.defaultImg = item.templateCover;
-        this.templateName = item.templateName;
-        this.templateDes = item.templateDes;
-        this.fileName = item.templateCover;
-        this.templateId = item.templateId;
-      } else {
-        this.defaultImg = "https://art1001-bim-5d.oss-cn-beijing.aliyuncs.com/upload/project/bj.png";
-        this.templateName = "";
-        this.templateDes = "";
-        this.fileName = "";
-      }
-    },
-    saveTemplate() {
-      if (this.templateId) {
-        this.editTempalte(this.templateId);
-      } else {
+      addTemplate(item) {
+        this.showTemplate = true;
+        if (item) {
+          this.defaultImg = item.templateCover;
+          this.templateName = item.templateName;
+          this.templateDes = item.templateDes;
+          this.fileName = item.templateCover;
+          this.templateId = item.templateId;
+        } else {
+          this.defaultImg = "https://art1001-bim-5d.oss-cn-beijing.aliyuncs.com/upload/project/bj.png";
+          this.templateName = "";
+          this.templateDes = "";
+          this.fileName = "";
+        }
+      },
+      saveTemplate() {
+        if (this.templateId) {
+          this.editTempalte(this.templateId);
+        } else {
+          if (!this.templateName) {
+            this.$Message.warning("请输入模板名称");
+            return false;
+          }
+          if (!this.templateDes) {
+            this.$Message.warning("请输入模板描述");
+            return false;
+          }
+          let data = {
+            templateName: this.templateName,
+            templateDes: this.templateDes,
+            templateCover: this.fileName ? this.fileName : this.defaultImg,
+            orgId: localStorage.companyId,
+          };
+          createTemplate(data).then((res) => {
+            if (res.result == 1) {
+              this.showTemplate = false;
+              this.initTemplate();
+              this.$Message.success("保存成功");
+            } else {
+              this.$Message.warning("保存失败");
+            }
+          });
+        }
+      },
+      editTempalte(templateId) {
         if (!this.templateName) {
           this.$Message.warning("请输入模板名称");
           return false;
@@ -157,12 +193,12 @@ export default {
           return false;
         }
         let data = {
+          templateId: templateId,
           templateName: this.templateName,
           templateDes: this.templateDes,
           templateCover: this.fileName ? this.fileName : this.defaultImg,
-          orgId: localStorage.companyId,
         };
-        createTemplate(data).then((res) => {
+        updateTemplate(data).then((res) => {
           if (res.result == 1) {
             this.showTemplate = false;
             this.initTemplate();
@@ -171,227 +207,200 @@ export default {
             this.$Message.warning("保存失败");
           }
         });
-      }
-    },
-    editTempalte(templateId) {
-      if (!this.templateName) {
-        this.$Message.warning("请输入模板名称");
-        return false;
-      }
-      if (!this.templateDes) {
-        this.$Message.warning("请输入模板描述");
-        return false;
-      }
-      let data = {
-        templateId: templateId,
-        templateName: this.templateName,
-        templateDes: this.templateDes,
-        templateCover: this.fileName ? this.fileName : this.defaultImg,
-      };
-      updateTemplate(data).then((res) => {
-        if (res.result == 1) {
-          this.showTemplate = false;
-          this.initTemplate();
-          this.$Message.success("保存成功");
+      },
+      deleteTemplate(templateId) {
+        let data = {
+          templateId: templateId
+        };
+        deleteTemplate(data).then((res) => {
+          if (res.result == 1) {
+            this.initTemplate();
+            this.$Message.success("删除成功");
+          } else {
+            this.$Message.warning("删除失败");
+          }
+        });
+      },
+      showTaskModal(template) {
+        this.showTaskList = true;
+        this.templateId = template.templateId;
+        this.templateName = template.templateName;
+      },
+      handleFormatError(file) {
+        this.$Message.warning({
+          title: "文件格式不正确",
+          desc: "文件格式不正确，请选择jpg或者png.",
+        });
+      },
+      handleMaxSize(file) {
+        this.$Message.warning({
+          title: "文件大小超出限制",
+          desc: "文件大小不能超过1M，请重新调整再上传",
+        });
+      },
+      handleBeforeUpload(file) {
+        let dir = ''
+        dir = "upload/template/" + this.$moment().format('YYYY-MM-DD') + "/";
+        return oss(dir, file.name).then(res => {
+          this.host = res.host;
+          this.uploadData = res;
+          this.fileObj=file
+        });
+      },
+      handleSuccess() {
+        const fileReader = new FileReader(); //内置方法new FileReader()   读取文件
+        fileReader.addEventListener("load", () => {
+          this.defaultImg = fileReader.result;
+        });
+        fileReader.readAsDataURL(this.fileObj);
+        //到这里后, 选择图片就可以显示出来了
+      },
+      changeVisible(value) {
+        if (value) {
+          this.initRelation();
+        }
+      },
+      showRelationAdd(relation) {
+        this.addRelation = true;
+        if (relation) {
+          this.relationName = relation.relationName;
+          this.relationOrder = relation.relationOrder;
+          this.relationId = relation.relationId;
         } else {
-          this.$Message.warning("保存失败");
+          this.relationName = '';
+          this.relationOrder = '0';
+          this.relationId = '';
         }
-      });
-    },
-    deleteTemplate(templateId) {
-      let data = { templateId: templateId };
-      deleteTemplate(data).then((res) => {
-        if (res.result == 1) {
-          this.initTemplate();
-          this.$Message.success("删除成功");
+
+      },
+      initRelation() {
+        initTemplateRelation({
+          templateId: this.templateId
+        }).then((res) => {
+          if (res.result == 1) {
+            this.relations = res.data;
+          }
+        });
+      },
+      saveRelation() {
+        if (!this.relationName) {
+          this.$Message.warning("请输入任务列表名称");
+          return false;
+        }
+        let data = {
+          templateId: this.templateId,
+          relationName: this.relationName,
+          relationOrder: this.relationOrder,
+        };
+        if (this.relationId) {
+          this.updateRelation();
         } else {
-          this.$Message.warning("删除失败");
+          createTemplateRelation(data).then((res) => {
+            if (res.result == 1) {
+              this.addRelation = false;
+              this.initRelation();
+              this.$Message.success("创建成功");
+            } else {
+              this.$Message.warning("创建失败");
+            }
+          });
         }
-      });
-    },
-    showTaskModal(template) {
-      this.showTaskList = true;
-      this.templateId = template.templateId;
-      this.templateName = template.templateName;
-    },
-    handleFormatError(file) {
-      this.$Message.warning({
-        title: "文件格式不正确",
-        desc: "文件格式不正确，请选择jpg或者png.",
-      });
-    },
-    handleMaxSize(file) {
-      this.$Message.warning({
-        title: "文件大小超出限制",
-        desc: "文件大小不能超过1M，请重新调整再上传",
-      });
-    },
-    handleBeforeUpload(file) {
-      const fileReader = new FileReader(); //内置方法new FileReader()   读取文件
-      fileReader.addEventListener("load", () => {
-        this.defaultImg = fileReader.result;
-      });
-      fileReader.readAsDataURL(file);
-      //到这里后, 选择图片就可以显示出来了
-      this.fileName = "upload/template/" + this.random_string(10) + this.get_suffix(this.filename);
-      client.multipartUpload(this.fileName, file);
-      return false;
-    },
-    random_string(len) {
-      len = len || 32;
-      var chars = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678";
-      var maxPos = chars.length;
-      var pwd = "";
-      for (var i = 0; i < len; i++) {
-        pwd += chars.charAt(Math.floor(Math.random() * maxPos));
-      }
-      return pwd;
-    },
-    get_suffix(filename) {
-      var pos = filename.lastIndexOf(".");
-      var suffix = "";
-      if (pos !== -1) {
-        suffix = filename.substring(pos);
-      }
-      return suffix;
-    },
-    changeVisible(value) {
-      console.log(value);
-      if (value) {
-        this.initRelation();
-      }
-    },
-    showRelationAdd(relation) {
-      this.addRelation = true;
-      if (relation) {
-        this.relationName = relation.relationName;
-        this.relationOrder = relation.relationOrder;
-        this.relationId = relation.relationId;
-      }else{
-        this.relationName = '';
-        this.relationOrder = '0';
-        this.relationId ='';
-      }
-      
-    },
-    initRelation() {
-      initTemplateRelation({ templateId: this.templateId }).then((res) => {
-        if (res.result == 1) {
-          this.relations = res.data;
+      },
+      updateRelation() {
+        if (!this.relationName) {
+          this.$Message.warning("请输入任务列表名称");
+          return false;
         }
-      });
-    },
-    saveRelation() {
-      if (!this.relationName) {
-        this.$Message.warning("请输入任务列表名称");
-        return false;
-      }
-      let data = {
-        templateId: this.templateId,
-        relationName: this.relationName,
-        relationOrder: this.relationOrder,
-      };
-      if (this.relationId) {
-        this.updateRelation();
-      } else {
-        createTemplateRelation(data).then((res) => {
+
+        let data = {
+          relationId: this.relationId,
+          relationName: this.relationName,
+          relationOrder: this.relationOrder,
+        };
+        updateTemplateRelation(data).then((res) => {
           if (res.result == 1) {
             this.addRelation = false;
             this.initRelation();
-            this.$Message.success("创建成功");
+            this.$Message.success("更新成功");
           } else {
-            this.$Message.warning("创建失败");
+            this.$Message.warning("更新失败");
           }
         });
-      }
+      },
+      deleteRelation(item) {
+        let data = {
+          relationId: item.relationId
+        };
+        deleteTemplateRelation(data).then((res) => {
+          if (res.result == 1) {
+            this.initRelation();
+            this.$Message.success("删除成功");
+          } else {
+            this.$Message.warning("删除失败");
+          }
+        });
+      },
     },
-    updateRelation() {
-      if (!this.relationName) {
-        this.$Message.warning("请输入任务列表名称");
-        return false;
-      }
-
-      let data = {
-        relationId: this.relationId,
-        relationName: this.relationName,
-        relationOrder: this.relationOrder,
-      };
-      updateTemplateRelation(data).then((res) => {
-        if (res.result == 1) {
-          this.addRelation = false;
-          this.initRelation();
-          this.$Message.success("更新成功");
-        } else {
-          this.$Message.warning("更新失败");
-        }
-      });
-    },
-    deleteRelation(item) {
-      let data = { relationId: item.relationId };
-      deleteTemplateRelation(data).then((res) => {
-        if (res.result == 1) {
-          this.initRelation();
-          this.$Message.success("删除成功");
-        } else {
-          this.$Message.warning("删除失败");
-        }
-      });
-    },
-  },
-};
+  };
 </script>
 <style lang="less" scoped>
-.template-div {
-  height: calc(100vh - 88px);
-  background: #fff;
-  border-radius: 4px;
-}
-.template-box {
-  width: 100%;
-  margin-bottom: 10px;
-  height: 100%;
-  header {
-    width: 100%;
-    height: 70px;
-    border-bottom: 1px solid #e5e5e5;
-    padding: 0 24px;
-    font-size: 16px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-}
-.template-content {
-  padding-left: 20px;
-  padding-right: 45px;
-  height: calc(100vh - 158px);
-  overflow: hidden;
-  overflow-y: auto;
-}
-.add-template-box {
-  img {
-    width: 240px;
-    height: 120px;
+  .template-div {
+    height: calc(100vh - 88px);
+    background: #fff;
     border-radius: 4px;
   }
-  .add-template-upload {
-    display: flex;
-    align-items: center;
-  }
-}
 
-.task-list-box {
-  width: 100%;
-  margin-bottom: 10px;
-  height: 100%;
-  header {
+  .template-box {
     width: 100%;
-    height: 30px;
-    padding: 0 20px;
-    font-size: 16px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+    margin-bottom: 10px;
+    height: 100%;
+
+    header {
+      width: 100%;
+      height: 70px;
+      border-bottom: 1px solid #e5e5e5;
+      padding: 0 24px;
+      font-size: 16px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
   }
-}
+
+  .template-content {
+    padding-left: 20px;
+    padding-right: 45px;
+    height: calc(100vh - 158px);
+    overflow: hidden;
+    overflow-y: auto;
+  }
+
+  .add-template-box {
+    img {
+      width: 240px;
+      height: 120px;
+      border-radius: 4px;
+    }
+
+    .add-template-upload {
+      display: flex;
+      align-items: center;
+    }
+  }
+
+  .task-list-box {
+    width: 100%;
+    margin-bottom: 10px;
+    height: 100%;
+
+    header {
+      width: 100%;
+      height: 30px;
+      padding: 0 20px;
+      font-size: 16px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+  }
 </style>
