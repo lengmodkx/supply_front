42312e30806c300c70d6484c0d46cdc11ff35d2f
yafeng wang <wangyafeng@1001.com>
2021-04-10 04:55:27 +0000

    Update index.js

--- src/admin/config/index.js
+++ src/admin/config/index.js
@@ -18,7 +18,7 @@ export default {
    */
   baseUrl: {
     dev: 'http://localhost:8091/api/',
-    pro: 'https://work.aldbim.com/api/'
+    pro: 'https://test.art1001.com/api/'
   },
   /**
    * @description 默认打开的首页的路由name值，默认为home
