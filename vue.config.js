42d9b6f4b772fee7cb7f66cfb1d0ad0cefa33208
kxDeng <lengmodkx@163.com>
2021-06-20 21:01:16 +0800

    阿拉丁BIM协同平台前端代码

--- vue.config.js
+++ vue.config.js
@@ -50,7 +50,8 @@ module.exports = {
         // proxy: 'http://localhost:4000'// 配置跨域处理,只有一个代理
         proxy: {
             "/api": {
-                target: "http://192.168.31.22:8080",
+                // target: "http://192.168.31.22:8080",
+                target: "http://localhost:8080",
                 ws: true,
                 changeOrigin: true,
                 pathRewrite: {
